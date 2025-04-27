cfd3e2e720f3161057a8b20f341915fb3e38c70e
2baSW <sows32000@gmail.com>
2025-04-27 14:03:27 +0200

    Méthode GET, POST, DELETE et PUT

--- TP3/spring1/src/main/java/com/example/spring1/MyApi.java
+++ TP3/spring1/src/main/java/com/example/spring1/MyApi.java
@@ -1,10 +1,45 @@
 package com.example.spring1;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 
 @RestController // Annotation pour dire que c'est un controller rest
 public class MyApi {
+
+    public static ArrayList<Etudiant> listEtudiant = new ArrayList<>();
+    static {
+        listEtudiant.add(new Etudiant(0, "A", 19));
+        listEtudiant.add(new Etudiant(1, "B", 17));
+        listEtudiant.add(new Etudiant(2, "C", 15));
+        listEtudiant.add(new Etudiant(3, "D", 13));
+    }
+    
+    @GetMapping(value = "/listEtudiant")
+    public ArrayList<Etudiant> getAllEtudiant() {
+        return listEtudiant;
+    }
+
+    @GetMapping(value = "/getEtudiant")
+    public Etudiant getEtudiant(int identifiant) {
+        return listEtudiant.get(identifiant);
+    }
+
+    @PostMapping(value = "/addEtudiant")
+    public Etudiant addEtudiant(Etudiant etudiant) {
+        listEtudiant.add(etudiant);
+        return etudiant;
+    }
+
+    @DeleteMapping(value = "/deleteEtudiant")
+    public void deleteEtudiant(int identifiant) {
+        listEtudiant.remove(identifiant);
+    }
+
+    @PutMapping(value = "/updateEtudiant")
+    public void UpdateEtudiant(int identifiant, String nom) {
+        listEtudiant.get(identifiant).setNom(nom);
+    }
 
     @GetMapping(value = "/b")
     public String bonjour() {
